240c3efe49e9b73ee613b2fd02d945226c450053
Michael Limiero <mike5713@gmail.com>
2015-05-01 15:27:58 -0400

    Update with a Skypebot hack

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "cinch"
+gem "cinchize"
+gem "nokogiri"
--- compass.rb
+++ compass.rb
@@ -1,17 +1,16 @@
 #!/usr/bin/ruby
 require 'cinch'
-require 'dbm'
 require './calc'
 require './ellipse'
-
-$db = DBM.open('compass.db', 0644, DBM::WRCREAT)
-online = {}
+require_relative 'plugins/link_info'
 
 bot = Cinch::Bot.new do
     configure do |c|
-        c.server = 'brick.miscjunk.net'
+        c.server = '$server'
+        c.password = '$password'
         c.nick = 'Compass'
-        c.channels = ['#minecraft']
+        c.channels = ['#$channel']
+        c.plugins.plugins = [Cinch::LinkInfo]
     end
 
     @rules = []
@@ -53,7 +52,15 @@ bot = Cinch::Bot.new do
     end
 
     on :message do |m|
-        direct = (m.message =~ /\Acompass[,:] (.+)\z/i)
+        if m.user.nick =~ /SkypeBot_*/
+          m.message =~ /\A\<([^>]+)\> (.+)\z/
+          message = $2
+          nick = $1.split[0]
+        else
+          message = m.message
+          nick = m.user.nick
+        end
+        direct = (message =~ /\Acompass[,:] (.+)\z/i)
         types = if !m.channel
                     [:private, :any]
                 elsif direct
@@ -61,55 +68,34 @@ bot = Cinch::Bot.new do
                 else
                     [:indirect, :any]
                 end
-        message = direct ? $1 : m.message
+        message = direct ? $1 : message
         bot.rules.each do |rule|
-            res = rule[:block].call(m, message) if bot.match(rule, message, *types)
+            res = rule[:block].call(m, message, nick) if bot.match(rule, message, *types)
             if res
                 m.reply res
                 break
             end
         end
     end
-
-    on :join do |m|
-        online[m.user.nick.downcase] = true
-        $db["seen:#{m.user.nick.downcase}"] = Time.now.to_i
-    end
-    on :leaving do |m|
-        online[m.user.nick.downcase] = false
-        $db["seen:#{m.user.nick.downcase}"] = Time.now.to_i
-    end
 end
 
-bot.rule any: /\Ahello compass\z/i, [:direct, :private] => "hello" do |m,cmd|
+bot.rule any: /\Ahello compass\z/i, [:direct, :private] => "hello" do |m,cmd,nick|
     greetings = ["Hello", "Hi", "Howdy", "Hey"]
-    "#{greetings.sample} #{m.user.nick}!"
+    "#{greetings.sample} #{nick}!"
 end
 
-bot.rule any: /\A!(circle|ellipse) ([0-9]+)( ([0-9]+))?\z/, [:direct, :private] => /\A!?(circle|ellipse) ([0-9]+)( ([0-9]+))?\z/ do |m,cmd|
+bot.rule any: /\A!(circle|ellipse) ([0-9]+)( ([0-9]+))?\z/, [:direct, :private] => /\A!?(circle|ellipse) ([0-9]+)( ([0-9]+))?\z/ do |m,cmd,nick|
     cmd =~ /\A!?(circle|ellipse) ([0-9]+)( ([0-9]+))?\z/
     xdia = $2.to_i
     ydia = $4 ? $4.to_i : xdia
     next if xdia > 1000 || ydia > 1000
     res = ellipse(xdia, ydia)
     next unless res
-    res = "#{m.user.nick}: #{res}" if m.message != cmd
+    res = "#{nick}: #{res}" if m.message != cmd
     res
 end
 
-bot.rule any: /\A!seen ([A-Za-z0-9_-]+)\z/ do |m, cmd|
-    cmd =~ /\A!seen ([A-Za-z0-9_-]+)\z/
-    player = $1
-    if online[player.downcase]
-        "#{$1} is online since "+ Time.at($db["seen:#{player.downcase}"].to_i).to_s
-    elsif $db["seen:#{player.downcase}"]
-        "#{$1} was last seen "+ Time.at($db["seen:#{player.downcase}"].to_i).to_s
-    else
-        "I've never seen #{$1}."
-    end
-end
-
-bot.rule any: /\A!roll ([0-9]*)d([0-9]+)\z/, [:direct, :private] => /\A!?roll ([0-9]*)d([0-9]+)\z/ do |m,cmd|
+bot.rule any: /\A!roll ([0-9]*)d([0-9]+)\z/, [:direct, :private] => /\A!?roll ([0-9]*)d([0-9]+)\z/ do |m,cmd,nick|
     cmd =~ /\A!?roll ([0-9]*)d([0-9]+)\z/
     n = $1.to_i
     n = 1 if n == 0
@@ -117,39 +103,34 @@ bot.rule any: /\A!roll ([0-9]*)d([0-9]+)\z/, [:direct, :private] => /\A!?roll ([
     sum = 0
     n.times { sum += Random.rand(1..sides) }
     res = sum.to_s
-    res = "#{m.user.nick}: #{res}" if m.message != cmd
+    res = "#{nick}: #{res}" if m.message != cmd
     res
 end
 
-bot.rule any: /\A!flip\z/, [:direct, :private] => /\A!?flip\z/ do |m,cmd|
+bot.rule any: /\A!flip\z/, [:direct, :private] => /\A!?flip\z/ do |m,cmd,nick|
     res = ["heads","tails"].sample
-    res = "#{m.user.nick}: #{res}" if m.message != cmd
+    res = "#{nick}: #{res}" if m.message != cmd
     res
 end
 
-bot.rule any: /\A!?botsnack\z/i do |m,cmd|
+bot.rule any: /\A!?botsnack\z/i do |m,cmd,nick|
     ":D"
 end
 
-bot.rule direct: /\A(.*) or (.*?)\??\z/i do |m,cmd|
+bot.rule direct: /\A(.*) or (.*?)\??\z/i do |m,cmd,nick|
     cmd =~ /\A(.*) or (.*?)\??\z/i
     [$1,$2].sample
 end
 
-bot.rule [:direct, :private] => // do |m,cmd|
+bot.rule [:direct, :private] => // do |m,cmd,nick|
     res = calc(cmd) rescue nil
     next unless res
-    res = "#{m.user.nick}: #{res}" if m.message != cmd
+    res = "#{nick}: #{res}" if m.message != cmd
     res
 end
 
-bot.rule [:direct, :private] => // do |m,cmd|
+bot.rule [:direct, :private] => // do |m,cmd,nick|
     "I don't know what you're talking about."
 end
 
 bot.start
-online.each do |(k,v)|
-    next unless v
-    $db["seen:#{k}"] = Time.now.to_i
-end
-$db.close
